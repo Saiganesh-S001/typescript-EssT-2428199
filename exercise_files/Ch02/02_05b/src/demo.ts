c1db86828f474e681996b6b71e208c28d9a0dce2
Saiganesh-S001 <saiganesh.s@ringcentral.com>
2025-01-20 09:01:04 +0000

    02_05b

--- exercise_files/Ch02/02_05b/src/demo.ts
+++ exercise_files/Ch02/02_05b/src/demo.ts
@@ -1,8 +1,19 @@
 interface Contact {
     id: number;
     name: string;
+    method(name:string) : void
 }
 
-function clone(source) {
+function clone(source:Contact, func:(name:string)=>void) : Contact {
     return Object.apply({}, source);
 }
+
+const printHello = (name:string) => console.log(name);
+
+let a : Contact = {
+    id:1,
+    name:"Hello",
+    method:printHello
+}
+
+let b = clone(a,printHello);
