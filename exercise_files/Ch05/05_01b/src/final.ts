d08fa2ebb89d46a5d1e0cbb04ddba62d1b9c6806
Saiganesh-S001 <saiganesh.s@ringcentral.com>
2025-01-27 16:58:40 +0000

    dec:method

--- exercise_files/Ch05/05_01b/src/final.ts
+++ exercise_files/Ch05/05_01b/src/final.ts
@@ -7,20 +7,37 @@ const currentUser = {
     roles: ["ContactEditor"],
     isInRole(role: string): boolean {
         return this.roles.contains(role);
+    },
+    isAuthenticated():boolean {
+        return true
     }
 }
 
-@log
+function authorize(target:any, property:string, descriptor:PropertyDescriptor){
+    const wrapper = descriptor.value
+    descriptor.value = () => {
+        if(!currentUser.isAuthenticated()){
+            throw Error("Not authorised")
+        }
+        try{
+            return wrapper.apply(this,arguments)
+        }catch(err){
+            return err
+        }
+    }
+
+}
+//implement log decarator
 class ContactRepository {
     private contacts: Contact[] = [];
 
-    @authorize("ContactViewer")
+    @authorize
     getContactById(id: number): Contact | null {
         const contact = this.contacts.find(x => x.id === id);
         return contact;
     }
 
-    @authorize("ContactEditor")
+    @authorize
     save(contact: Contact): void {
         const existing = this.getContactById(contact.id);
 
