3845c0037127cf0eac66ae2b31385864962b598b
Saiganesh-S001 <saiganesh.s@ringcentral.com>
2025-01-30 04:58:49 +0000

    Typescript done

--- exercise_files/Ch05/05_01b/src/final.ts
+++ exercise_files/Ch05/05_01b/src/final.ts
@@ -8,42 +8,43 @@ const currentUser = {
     isInRole(role: string): boolean {
         return this.roles.contains(role);
     },
-    isAuthenticated():boolean {
+    isAuthenticated(): boolean {
         return true
     }
 }
 
-function authorize(role:string) {
-    return function authorizeDecorator(target:any, property:string, descriptor:PropertyDescriptor){
+
+function authorize(role: string) {
+    return function authorizeDecorator(target: any, property: string, descriptor: PropertyDescriptor) {
         const wrapper = descriptor.value
         descriptor.value = () => {
-            if(!currentUser.isAuthenticated() || !currentUser.isInRole(role)){
+            if (!currentUser.isAuthenticated() || !currentUser.isInRole(role)) {
                 throw Error("Not authorised")
             }
-            try{
-                return wrapper.apply(this,arguments)
-            }catch(err){
+            try {
+                return wrapper.apply(this, arguments)
+            } catch (err) {
                 return err;
             }
         }
-    
+
     }
 }
 
 // target is the constructor function of the class
 // class decarotor - to change classes dynamically 
-function freeze(target: Function){
+function freeze(target: Function) {
     Object.freeze(target)
     Object.freeze(target.prototype)
 }
 
-function singleton<T extends {new (...args:any[]):{}}>(constructor: T){
+function singleton<T extends { new(...args: any[]): {} }>(constructor: T) {
     return class Singleton extends constructor {
         static _instance = null
-        constructor(...args){
+        constructor(...args) {
             super(...args)
-            
-            if(Singleton._instance != null){
+
+            if (Singleton._instance != null) {
                 throw Error("There exists an instance for this class")
             }
             Singleton._instance = this
